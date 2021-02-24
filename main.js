61bcf9c7e7249bc73feea2595e8908fccf454754
BitInByte <bitinbyte@outlook.com>
2021-02-24 15:02:21 +0000

    Added more tasks

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const { task1, task2, task3 } = require("./tasks.js");
+const { task1, task2, task3, task4, task5, task6, task7 } = require("./tasks.js");
 
 console.log("Task 1: ", task1(3, 'a'));
 console.log("Task 2 : ", task2([1, 2, 3, 4]));
 console.log("Task 3: ", task3(['a', 3, undefined, null, '']));
+console.log("Task 4: ", task4([["c",2],["d",4]]));
+console.log("Task 5: ", task5([1, 3, 2, 6, 7, 5], 5, 2))
+console.log("Task 6: ", task6([1,2,3,3,2,4,5,4,7,3]))
+console.log("Task 7: ", task7([1,2,3,4],[1,2,3,4,5]));
--- tasks.js
+++ tasks.js
@@ -20,12 +20,72 @@ const task2 = (arr) => {
 const task3 = (arr) => {
   if(!arr || !Array.isArray(arr)) return "ERROR: Invalid Array!";
 
+  // Filter filters a value, since undefined, null, empty strings
+  // and so on are not a valid value, filter doesn't return that.
   return arr.filter(value => value);
 
 }
+
+const task4 = (arr) => {
+  if(!arr || !Array.isArray(arr)) return "ERROR: Invalid Array!";
+
+  // Since the array have 2 entries in each array inside, fromEntries
+  // will convert each array into a key value pairs which will use the first
+  // entry as a key and the second entry as a value
+  return Object.fromEntries(arr);
+}
+
+const task5 = (arr, ...args) => {
+  if(!arr || !Array.isArray(arr)) return "ERROR: Invalid Array!";
+
+  // By deafult, arrays have the method includes which return true or false
+  // of the element is on the array, combined with filter we can select
+  // an array of arguments to check if they are on the first array argument
+  return arr.filter(el => !args.includes( el ));
+}
+
+const task6 = (arr) => {
+  if(!arr || !Array.isArray(arr)) return "ERROR: Invalid Array!";
+
+  // Since a set doens't allow duplicated values, we can set a new
+  // set based on the array provided and then spread it into a new array
+  return [...new Set(arr)];
+
+}
+
+const task7 = (arr1, arr2) => {
+  if(!arr1 || !Array.isArray(arr1) || !arr2 || !Array.isArray(arr2)) return "ERROR: Invalid Array!";
+
+  // Since arrays are reference types, we cannot compare them with equality
+  // operation because they old a reference in memory, so, to perform
+  // this type of operation, we can convert them into string, since strings
+  // are primitive types and equality operations are allowed between
+  // primitive types
+
+  return arr1.toString() === arr2.toString();
+}
+
+const task8 = (arr) => {
+  if(!arr || !Array.isArray(arr)) return "ERROR: Invalid Array!";
+
+  return arr.map(elem => {
+    if(elem.length) {
+      const array = [];
+      x = 15;
+
+
+    }
+  })
+}
+
 
 module.exports = {
   task1,
   task2,
   task3,
+  task4,
+  task5,
+  task6,
+  task7,
 }
+
